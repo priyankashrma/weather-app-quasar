b221dcba65f80a99416db534390fceb6af380203
Priyanka Sharma <58728994+priyankashrma@users.noreply.github.com>
2021-05-31 11:07:16 +0530

    Weather app in Quasar and vue

---
+++ src/pages/Index.vue
@@ -0,0 +1,129 @@
+<template>
+  <q-page class="flex column" :class="bgClass">
+    <div class="col q-pt-lg q-px-md">
+      <q-input bottom-slots
+               v-model="search"
+               @keyup.enter="getWeatherBySearch"
+               placeholder="Search"
+               dark
+               borderless
+      >
+        <template v-slot:before>
+          <q-icon @click="getLocation" name="my_location"/>
+        </template>
+
+
+        <template v-slot:append>
+          <q-btn  @click="getWeatherBySearch" round dense flat icon="search"/>
+        </template>
+      </q-input>
+    </div>
+
+    <template v-if="weatherData">
+      <div class="col text-white text-center">
+        <div class="text-h4 text-weight-light">
+          {{weatherData.name}}
+        </div>
+        <div class="text-h6 text-weight-light">
+          {{weatherData.weather[0].main}}
+        </div>
+        <div class="text-h1 text-weight-thin q-my-lg relative-position">
+          <span>{{Math.round(weatherData.main.temp)}}</span>
+          <span class="text-h4 relative-position degree">&deg;C</span>
+        </div>
+      </div>
+      <div class="col text-center">
+        <img :src="`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`" alt="weather" />
+      </div>
+    </template>
+    <template v-else>
+      <div class="col column text-center text-white">
+        <div class="col text-h2 text-weight-thin">
+          Quasar<br/>Weather
+        </div>
+        <q-btn color="teal" class="col" flat @click="getLocation">
+          <q-icon left size="3em" name="my_location" />
+          <div>Find My Location</div>
+        </q-btn>
+
+      </div>
+    </template>
+    <div class="col skyline"></div>
+
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data() {
+    return {
+      search: "",
+      weatherData: null,
+      lat:null,
+      lon:null,
+      apiUrl:'https://api.openweathermap.org/data/2.5/weather'
+    }
+  },
+  computed:{
+    bgClass(){
+      if(this.weatherData){
+        if(this.weatherData.weather[0].icon.endsWith('n')){
+          return 'bg-night'
+        }
+        else{
+          return 'bg-day'
+        }
+      }
+    }
+  },
+
+  methods:{
+    getLocation(){
+      this.$q.loading.show()
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          console.log('position:', position)
+          this.lat=position.coords.latitude
+          this.lon=position.coords.longitude
+          this.getWeatherByCoords()
+          this.$q.loading.hide()
+        }
+      )
+    },
+    getWeatherByCoords(){
+      this.$q.loading.show()
+      this.$axios(`${this.apiUrl}?lat=${this.lat}&lon=${this.lon}&appid=3acc5005d6fe49cae6f716ff28ded903&units=metric`)
+      .then(response=> {
+        this.weatherData=response.data
+        console.log('resp', response)
+        this.$q.loading.hide()
+      })
+    },
+    getWeatherBySearch(){
+      this.$q.loading.show()
+      this.$axios(`${this.apiUrl}?q=${this.search}&appid=3acc5005d6fe49cae6f716ff28ded903&units=metric`)
+        .then(response=> {
+          this.weatherData=response.data
+          console.log('resp', response)
+          this.$q.loading.hide()
+        })
+    }
+  }
+}
+</script>
+<style lang="sass">
+.q-page
+  background: linear-gradient(to bottom, #136a8a, #267871)
+  &.bg-night
+  background: linear-gradient(to bottom , #232526,#414345)
+  &.bg-day
+  background: linear-gradient(to bottom , #00b4db,#0083b0)
+.degree
+  top: -44px
+
+.skyline
+  flex: 0 0 100px
+  background-size: contain
+  background: url("https://freepngimg.com/thumb/categories/3036.png") center bottom
+</style>
